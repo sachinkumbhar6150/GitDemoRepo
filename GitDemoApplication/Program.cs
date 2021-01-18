c33a3ac0140d36265f8d1eea772b8906d7d27a9a
sachinkumbhar6150 <sachin.kumbhar6150@gmail.com>
2021-01-18 20:13:33 +0530

    new code added

--- GitDemoApplication/Program.cs
+++ GitDemoApplication/Program.cs
@@ -17,6 +17,7 @@ namespace GitDemoApplication
             CreateWebHostBuilder(args).Build().Run();
         }
 
+
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
