662a241dcc39567bd52fb05cffd240fcbd44e280
sachinkumbhar6150 <sachin.kumbhar6150@gmail.com>
2021-01-18 20:06:51 +0530

    Add project files.

---
+++ GitDemoApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitDemoApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
